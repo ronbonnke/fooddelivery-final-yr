ff6d6ef49dcaa11e233032daa8c06d86355ab7ac
ron bonnke <ronbonnke789@gmail.com>
2026-03-23 10:24:19 +0530

    initil commit

---
+++ backend/config/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () =>{
+    await mongoose.connect('mongodb://127.0.0.1:27017/fooddelivery').then(()=>{
+       console.log('DB connected') ;
+    })
+}
